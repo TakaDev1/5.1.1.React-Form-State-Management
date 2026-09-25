3ab7d1ba5c9bb6742ad675e872fbeefe57ecbbda
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-25 15:59:34 +0900

    style: Project Adjustment Design

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import FormPage from "./pages/FormPage";
 function App() {
   return (
     <>
-      <FormPage />
+      <div className="min-h-screen flex flex-col justify-center bg-gray-800">
+        <h1>5.1.1.React-Form-State-Management</h1>
+        <FormPage />
+      </div>
     </>
   );
 }
--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -7,11 +7,21 @@ const FormPage = () => {
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="text">
-          <input type="text" value={inputValue} onChange={handleInput} />
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInput}
+            className="border text-white bg-gray-600"
+          />
         </label>
 
-        <p>入力値: {inputValue}</p>
-        <button type="submit">送信</button>
+        <p className="my-2">入力値: {inputValue}</p>
+        <button
+          type="submit"
+          className="bg-blue-300 text-black font-bold w-1/4 py-2 rounded-full hover:opacity-80 cursor-pointer"
+        >
+          送信
+        </button>
       </form>
       {submittedValue && <div>{submittedValue}</div>}
     </div>
